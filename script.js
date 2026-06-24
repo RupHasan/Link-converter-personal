2dbedb512c2451c59f3480f39a4980d5042fa3a6
Rup Hasan <hasanrup7@gmail.com>
2026-06-24 12:57:41 +0600

    fixed the bug in js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // What we will get : https://github/RupHasan/Git-learning/
 // What we have to give : https://ruphasan.github.io/Git-learning/
-function change() {
+function changeUrlFunction() {
   let mainUrl = document.querySelector("#get-url").value;
   let urlArr = mainUrl.split("/")
   let userName = urlArr[3].toLowerCase();
   let repoName = urlArr[4];
-  let newUrl = `https://${userName}.github.io/${repoName}/`
-  
+  let newUrl = `https://${userName}.github.io/${repoName}/`;
   
   document.querySelector(".container").innerHTML = `
-    <a href="${newUrl}><button class="new-url">Go to ${repoName}</button></a>
-  `
+    <a href="${newUrl}"><button class="new-url">Go to ${repoName}</button></a>
+  `;
+  document.querySelector("#get-url").value = "";
 }
