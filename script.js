e21350b09de7e4e90a820eba583ab034b56efcbe
Rup Hasan <hasanrup7@gmail.com>
2026-06-24 12:41:34 +0600

    Made the js properly but there is a bug. The button that should run the change function isnt working. will fix the but very soon

--- script.js
+++ script.js
@@ -1,32 +1,14 @@
-// // What we will get : https://github/RupHasan/Git-learning/
-// // What we have to give : https://ruphasan.github.io/Git-learning/
-// function change() {
-//   // Plan: 
-//   // 1. I will make it a aray and ignore the https://github/ part
-//   // 2. gotta save the username some way
-//   // 3. gotta save the repo name.
-//   // 4. we have to differ those from the main url.
-//   let mainUrl = document.querySelector("#get-url").value;
+// What we will get : https://github/RupHasan/Git-learning/
+// What we have to give : https://ruphasan.github.io/Git-learning/
+function change() {
+  let mainUrl = document.querySelector("#get-url").value;
+  let urlArr = mainUrl.split("/")
+  let userName = urlArr[3].toLowerCase();
+  let repoName = urlArr[4];
+  let newUrl = `https://${userName}.github.io/${repoName}/`
   
-//   console.log(mainUrl)
-//   let urlArr = mainUrl.split("")
-//   console.log(urlArr)
-// }
-
-
-
-
-// Lets practice regex first
-
-console.log(/github\.com\/RupHasan/.test("https://github.com/RupHasan"));
-// /products/12345/
-console.log("/products/12345/".match(/\d+/))
-// [01-02-2026] error
-console.log("[01-02-2026] error".match(/\[([^\]]+)\]/));
-
-// Now for the final boss
-
-
-let username = "https://ruphasan.github.io/Git-learning/".split("/")
-console.log(username)
-
+  
+  document.querySelector(".container").innerHTML = `
+    <a href="${newUrl}><button class="new-url">Go to ${repoName}</button></a>
+  `
+}
